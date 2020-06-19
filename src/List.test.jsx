ec0617d37f37cae7f3b0136a187019f45ecec116
yeonhwakim <yeonhwa6717@gmail.com>
2020-06-19 13:20:41 +0900

    List 컴포넌트 테스트
    - 할 일이 없으면 '할 일이 없어요!' 보여주기
    - 할 일이 있으면 목록 보여주기

---
+++ src/List.test.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import List from './List';
+
+
+describe('할 일이 있으면 목록을 보여주고, 없으면 할 일이 없어요! 라고 보여준다', () => {
+  test('할 일이 없으면 할 일이 없다고 보여주기', () => {
+    const tasks = [];
+    const { container } = render((
+      <List
+        tasks={tasks}
+      />
+    ));
+
+    expect(container).toHaveTextContent('할 일이 없어요!');
+  });
+
+  test('할 일이 있으면 목록보여주기', () => {
+    const tasks = [{
+      id: 1,
+      title: '책읽기',
+    }];
+
+    const { container } = render((
+      <List
+        tasks={tasks}
+      />
+    ));
+
+    expect(container).toHaveTextContent('책읽기');
+  });
+});
